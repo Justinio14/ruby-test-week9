6259fcdeeb63c7af629fa21fcabbcee19f17f4fe
apostolistax <a.taxidaridis@gmail.com>
2014-05-23 13:01:16 +0100

    passing get_first_half_of_string test

--- questions.rb
+++ questions.rb
@@ -1,6 +1,6 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
-  array.select { |elem| elem[0] == 'a' }
+  array.select { |elem| elem.start_with? 'a' }
 end
 
 # keep only the elements that start with a vowel
@@ -35,21 +35,31 @@ end
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array[3..-1]
+  # array = array[3..-1] retarded way
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.sort_by {|x| x[-1]}
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  half = string.length / 2
+  if string.length.even?
+    string[0..half-1]
+  else
+    string[0..half]
+  end
 end
 
 # turn a positive integer into a negative integer. A negative integer
